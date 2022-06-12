8bdec0ee90b79bb6f5393f308fc166acd0fe0ae1
dawidos234 <dawidos234@gmail.com>
2022-06-12 17:25:25 +0200

    latest Player changes

--- logic/src/main/java/pl/pwr/ludoSimulator/logic/Player.java
+++ logic/src/main/java/pl/pwr/ludoSimulator/logic/Player.java
@@ -1,14 +1,6 @@
 package pl.pwr.ludoSimulator.logic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Player  {
-    private List<Pawn> basePawns = new ArrayList<>(Arrays.asList(new Pawn(), new Pawn(), new Pawn(), new Pawn()));
-    private final List<Pawn> activePawns = new ArrayList<>();
-    private final List<Pawn> endPawns = new ArrayList<>();
-
     private int id;
     private int startPosition;
     private final int endPosition;
