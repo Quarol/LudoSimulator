cbfd6946f1c4a9137e2d13a1f300475d2cd66e27
dawidos234 <dawidos234@gmail.com>
2022-06-13 20:17:21 +0200

    ask for amount of players

--- application/src/main/java/pl/pwr/ludoSimulator/application/Application.java
+++ application/src/main/java/pl/pwr/ludoSimulator/application/Application.java
@@ -12,20 +12,18 @@ import java.util.Scanner;
 public class Application {
     public static void main (String[] args) {
 
-        /*
         Scanner scanner = new Scanner(System.in);
         System.out.println("Wpisz dane do symulacji");
         System.out.print("Liczba graczy: ");
-        nrOfPLayers = scanner.nextInt();
+        int nrOfPLayers = scanner.nextInt();
         System.out.println("\n");
-        int nrOfPLayers;
-        */
+
         List<Action> actions = new ArrayList<>();
         actions.add(new TakeOutPawnAction());
         actions.add(new MoveActivePawnAction());
         actions.add(new MoveEndPawnAction());
         actions.add(new KillPawnAction());
-        Board board = new BoardInitializer(4).initialize();
+        Board board = new BoardInitializer(nrOfPLayers).initialize();
         Display display = new Display(board);
         Simulation simulation = new Simulation(board, actions, new Callback(display));
         simulation.start();
