d7486972e34ecc2aca3713aead5d259d28b51162
Quarol <karol.charchut@gmail.com>
2022-06-22 19:54:08 +0200

    Bład w ActivePositions

--- display/src/main/java/pl/pwr/ludoSimulator/display/ActivePositions.java
+++ display/src/main/java/pl/pwr/ludoSimulator/display/ActivePositions.java
@@ -9,6 +9,8 @@ public class ActivePositions {
     private final List<DisplayPosition> positions = new ArrayList<>();
     private final int X_SEPARATOR = 2;
     private final int Y_SEPARATOR = 1;
+    private static final int MIN_POSITION = 1;
+    private static final int MAX_POSITION = 5;
     public ActivePositions() {
         this.generatePositions(new DisplayPosition(0,4), new DisplayPosition(10, 0));
         this.generatePositions(new DisplayPosition(12,0), new DisplayPosition(20, 5));
@@ -21,14 +23,20 @@ public class ActivePositions {
         int y = player1.y();
         positions.add(new DisplayPosition(x, y));
         if (x < player2.x() && y > player2.y()) {
-            positions.add(new DisplayPosition(x+1*X_SEPARATOR, y));
+            /*positions.add(new DisplayPosition(x+1*X_SEPARATOR, y));
             positions.add(new DisplayPosition(x+2*X_SEPARATOR, y));
             positions.add(new DisplayPosition(x+3*X_SEPARATOR, y));
-            positions.add(new DisplayPosition(x+4*X_SEPARATOR, y));
+            positions.add(new DisplayPosition(x+4*X_SEPARATOR, y));*/
+
+
+            loop(x, MIN_POSITION, MAX_POSITION-1, 1, y, 0, 0, 0);
+            //positions.addAll(loop(x, MIN_POSITION, MAX_POSITION, 1, 0, 0, 0, 0));
+
             positions.add(new DisplayPosition(x+4*X_SEPARATOR, y-Y_SEPARATOR));
             positions.add(new DisplayPosition(x+4*X_SEPARATOR, y-2*Y_SEPARATOR));
             positions.add(new DisplayPosition(x+4*X_SEPARATOR, y-3*Y_SEPARATOR));
             positions.add(new DisplayPosition(x+4*X_SEPARATOR, y-4*Y_SEPARATOR));
+
             positions.add(new DisplayPosition(x+5*X_SEPARATOR, y-4*Y_SEPARATOR));
         }
         if (x < player2.x() && y < player2.y()) {
@@ -36,10 +44,12 @@ public class ActivePositions {
             positions.add(new DisplayPosition(x, y+2*Y_SEPARATOR));
             positions.add(new DisplayPosition(x, y+3*Y_SEPARATOR));
             positions.add(new DisplayPosition(x, y+4*Y_SEPARATOR));
+
             positions.add(new DisplayPosition(x+1*X_SEPARATOR, y+4*Y_SEPARATOR));
             positions.add(new DisplayPosition(x+2*X_SEPARATOR, y+4*Y_SEPARATOR));
             positions.add(new DisplayPosition(x+3*X_SEPARATOR, y+4*Y_SEPARATOR));
             positions.add(new DisplayPosition(x+4*X_SEPARATOR, y+4*Y_SEPARATOR));
+
             positions.add(new DisplayPosition(x+4*X_SEPARATOR, y+5*Y_SEPARATOR));
         }
         if (x > player2.x() && y < player2.y()) {
@@ -47,10 +57,12 @@ public class ActivePositions {
             positions.add(new DisplayPosition(x-2*X_SEPARATOR, y));
             positions.add(new DisplayPosition(x-3*X_SEPARATOR, y));
             positions.add(new DisplayPosition(x-4*X_SEPARATOR, y));
+
             positions.add(new DisplayPosition(x-4*X_SEPARATOR, y+1*Y_SEPARATOR));
             positions.add(new DisplayPosition(x-4*X_SEPARATOR, y+2*Y_SEPARATOR));
             positions.add(new DisplayPosition(x-4*X_SEPARATOR, y+3*Y_SEPARATOR));
             positions.add(new DisplayPosition(x-4*X_SEPARATOR, y+4*Y_SEPARATOR));
+
             positions.add(new DisplayPosition(x-5*X_SEPARATOR, y+4*Y_SEPARATOR));
         }
         if (x > player2.x() && y > player2.y()) {
@@ -58,12 +70,24 @@ public class ActivePositions {
             positions.add(new DisplayPosition(x, y-2*Y_SEPARATOR));
             positions.add(new DisplayPosition(x, y-3*Y_SEPARATOR));
             positions.add(new DisplayPosition(x, y-4*Y_SEPARATOR));
+
             positions.add(new DisplayPosition(x-1*X_SEPARATOR, y-4*Y_SEPARATOR));
             positions.add(new DisplayPosition(x-2*X_SEPARATOR, y-4*Y_SEPARATOR));
             positions.add(new DisplayPosition(x-3*X_SEPARATOR, y-4*Y_SEPARATOR));
             positions.add(new DisplayPosition(x-4*X_SEPARATOR, y-4*Y_SEPARATOR));
+
             positions.add(new DisplayPosition(x-4*X_SEPARATOR, y-5*Y_SEPARATOR));
         }
+    }
+
+    private List<DisplayPosition> loop(int x, int minX, int maxX, int xInterval,
+                                      int y, int minY, int maxY, int yInterval) {
+        List<DisplayPosition> displayPositions = new ArrayList<>();
+
+        for (int i = minX, j = minY; i <= maxX || j <= maxY; i += xInterval, j += yInterval)
+            positions.add(new DisplayPosition(x + i*X_SEPARATOR, y + j*Y_SEPARATOR));
+
+        return displayPositions;
     }
 
     public DisplayPosition get(int index) {
